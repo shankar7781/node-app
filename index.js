fb57aef5ffa693531da4f167b1c8d7e53c049238
Ubuntu <ubuntu@ip-172-31-14-212.ap-south-1.compute.internal>
2024-04-14 03:33:03 +0000

    added

---
+++ index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Home page of assignment'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80 , mention your port specific
+
